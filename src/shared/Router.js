46c2a51f34a92064b1814be9d5e1722f1256f763
jiapril11 <tadadadacoding@gmail.com>
2023-06-22 15:02:43 +0900

    헤더,푸터, 레이아웃 컴포넌트 생성

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -2,14 +2,17 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TodoApp from "../pages/TodoApp";
 import Todo from "../pages/Todo";
+import Layout from "../components/Layout";
 
 export default function Router() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<TodoApp />} />
-        <Route path="/todo/:id" element={<Todo />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<TodoApp />} />
+          <Route path="/todo/:id" element={<Todo />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 }
